930e9c43927fb8903e7e3f02a673548bd7f4a1cf
Ianadika44 <adika19ian@gmail.com>
2020-04-11 20:30:55 +0300

    added the github username to find out the data

--- src/app/services/profile.service.ts
+++ src/app/services/profile.service.ts
@@ -1,25 +1,27 @@
 import { Injectable } from '@angular/core';
 import { HttpClient }from '@angular/common/http';
-// import 'rxjs/add/operator/map'/
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProfileService {
 
-  private clientid = 'c9efe03ec420135763e3'
-  private clientsecret = 'e14bec4afe07b454e248c3e309d2c0b85cf289db'
-  username:string;
+  private  username:string;
+  private clientid = 'c9efe03ec420135763e3';
+  private clientsecret = 'e14bec4afe07b454e248c3e309d2c0b85cf289db';
+  map: any;
+ 
 
  
 
   constructor(private http:HttpClient) {
-    console.log("Your service are ready")
-    this.username = 'Adika'
+    console.log("Your service is ready");
+    this.username = 'Ianadika44';
    }
   
    getProfileInfo(){
      return this.http.get("https://api.github.com/users/" + this.username + "?clientid" + this.clientid + "&client_secret=" + this.clientsecret)
-    //  .map(res=>res.json)
+    
    }
 }
